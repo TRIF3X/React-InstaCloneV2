76e808fda12ba1ecd6eabb973b8c7ec91a1bbbd1
TRIF3X <thewizardofcrypto@gmail.com>
2018-12-31 09:22:52 -0600

    display each post with styles

--- src/components/PostContainer/Post.js
+++ src/components/PostContainer/Post.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import like from './likeandcomment.PNG'
 import './Post.css'
+
 
 function Post(props) {
     return (
-        <div className='postContainer'>
-            {props.post.username}
-        </div>
+        <div className='masterDiv'>
+            <div className='containerDiv'>
+                <h4 className='headerH4'><img className='thumbnail' src={props.post.thumbnailUrl} alt='header thumbnail'/>{props.post.username}</h4>
+                    <img className='image' src={props.post.imageUrl} alt='user post' />
+                    <img className='likeandcommentIcons' src={like} alt='like button' />
+                    <p className='likes'>Likes</p>           
+            </div>
+    </div>
     )
 }
 
