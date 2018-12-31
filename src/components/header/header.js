65b8c280f4b5d3ecc15eed50f98fa5a647037315
TRIF3X <thewizardofcrypto@gmail.com>
2018-12-31 10:36:45 -0600

    refactor existing components, grouped images, remove styled components

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,38 +1,15 @@
 import React from 'react';
-import logo from './instagramLogo.PNG'
-import links from './instagramSearchBarLinks.PNG'
-import styled from 'styled-components'
-
-const SearchBarDiv = styled.div`
-     display: flex;
-    justify-content: space-between;
-    position: fixed;
-    top: 0px;
-    background: white;
-    z-index: 2;
-    width: 100%;
-    border-bottom: 1px solid grey;
-
-`;
-const InstaLinks = styled.img`
-    margin-right: 18%;
-`
-const SearchField = styled.input`
-    color: grey;
-    text-align: center;
-    height: 50%;
-    margin-top: 3%;
-`
-const InstaLogo = styled.img`
-    margin-left: 18%;
-`
+import logo from './img/instagramLogo.PNG'
+import links from './img/instagramSearchBarLinks.PNG'
+import './header.css'
 
 const SearchBar = props => {
     return (
-        <SearchBarDiv>
-            <InstaLogo  src={logo} alt='instagram logo' />
+        <div className='searchBarDiv'>
+            <img className='instaLogo' src={logo} alt='instagram logo' />
             <form>
-                <SearchField
+                <input
+                 className='searchField'
                  type='text' 
                  placeholder='username'
                  name='search' 
@@ -40,9 +17,8 @@ const SearchBar = props => {
                  onChange={props.handleChange} 
                   />
             </form>
-            <InstaLinks src={links} alt='instagram links' />
-
-        </SearchBarDiv>
+            <img className='searchBarLinks'src={links} alt='search bar links' />
+        </div>
     )
 }
 
